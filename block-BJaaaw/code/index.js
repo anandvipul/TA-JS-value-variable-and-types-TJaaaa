c5d0bf9a9799dab89447dbff2b1a500f1a8e1171
anandvipul <anandvipul01@gmail.com>
2022-09-11 18:08:42 +0530

    Decision Making Assignment

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -7,9 +7,34 @@
   - `if` the number is odd print the message "number is odd"
 */
 
+let num = Number(prompt("Enter a Number: "));
+let rem = num / 2;
+
+switch (rem) {
+  case 0:
+    alert("Number is Even!");
+    break;
+
+  default:
+    alert("Number is odd");
+    break;
+}
+
 // 2. Write a program to accept two numbers from user using`prompt` and alert the max value.
 
+let num1 = Number("Enter a Number: ");
+let num2 = Number(prompt("Enter Another Number"));
+if (num2 > num1) {
+  alert("The Second Number is Greater.");
+} else {
+  alert("First Number is greater");
+}
+
 // 3. Convert the above code using`?` terniary operator
+
+num1 > num2
+  ? alert("First Number is greater")
+  : alert("Second Number is greater");
 
 /*
 4. Write a program that asks the user for the house name and check the following conditions:
@@ -19,7 +44,27 @@
   - `else` print the message " All men must die"
 */
 
+switch (prompt("Enter Your House Name: ")) {
+  case "stark":
+    alert("Winter is Coming");
+    break;
+
+  case "lannister":
+    alert("A lannister always pays his debt");
+    break;
+
+  default:
+    alert("All men must die");
+    break;
+}
+
 // 5. Convert the above code using`?` terniary operator
+let houseName = prompt("Enter name of Your House: ");
+houseName == "stark" || houseName == "lannister"
+  ? houseName == "stark"
+    ? alert("WInter is Coming")
+    : alert("A Lannister always pays his debt")
+  : alert("All Men Must die");
 
 // Switch
 
